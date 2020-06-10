f17d82316133c2b9933f42ed62362fd161a138a2
userv <rosen.t.angelov@gmail.com>
2020-06-10 12:43:41 +0300

    Refactored HomeController

--- PANDA/PANDA/Controllers/HomeController.cs
+++ PANDA/PANDA/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using PANDA.Data;
 using PANDA.Models;
@@ -13,14 +14,23 @@ namespace PANDA.Controllers
     public class HomeController : Controller
     {
         private readonly PandaDbContext dbContext;
+        private readonly UserManager<PandaUser> userManager;
 
-        public HomeController(PandaDbContext dbContext)
+        public HomeController(PandaDbContext dbContext,UserManager<PandaUser> userManager)
         {
             this.dbContext = dbContext;
+            this.userManager = userManager;
         }
         public IActionResult Index()
         {
-            var packages = this.dbContext.Packages.Select(p => new PackageHomeViewModel()
+            if (!this.User.Identity.IsAuthenticated)
+            {
+                return this.View( new List<PackageHomeViewModel>{});
+            }
+            var userId = this.userManager.GetUserId(this.User);
+            var packages = this.dbContext.Packages
+                .Where(p => p.RecipientId==userId)
+                .Select(p => new PackageHomeViewModel()
             {
                 Id = p.Id,
                 Description = p.Description,
